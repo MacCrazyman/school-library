aa8df3f766d46769333877e340caa4dc55c472b8
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-28 19:23:11 -0500

    Fix linters

--- Person.rb
+++ Person.rb
@@ -1,19 +1,26 @@
 class Person
   attr_reader :id
   attr_accessor :name, :age
-  def initialize name = unknown, age, parent_permission = true
+
+  def initialize(age, name = 'unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  
+
   def can_use_services?
-    is_of_age? || parent_permission
+    of_age? || parent_permission
   end
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
+
+# leo = Person.new 20, 'Leonardo'
+# p leo.can_use_services?
+# p leo.name
+# p leo.age
+# p leo.id
