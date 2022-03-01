fe5db2131ff3c4531a7a2b812f83b9e3cf85bf6a
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-03-01 14:09:44 -0500

    Add parent_permission argument to Teacher class

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'unknown')
-    super(age, name)
+  def initialize(age, specialization, name = 'unknown', parent_permission = true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
