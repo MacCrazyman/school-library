03dfebe26de24cf1d24b3b3484d156adc77c8880
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-03-02 16:26:04 -0500

    Add relationship between classroom and student

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+class Classroom
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    private
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end
